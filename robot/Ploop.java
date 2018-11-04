ac542d8ef70e314995d038fa89726e2fe41256bd
Aditi <abpv8@k12albemarle.org>
2018-11-04 16:35:40 -0500

    first ploop and hanger test code.

--- robot/Ploop.java
+++ robot/Ploop.java
@@ -1,22 +1,32 @@
 package rovr.robot;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import rovr.util.Param;
 
 public class Ploop extends Mechanism {
+    DcMotor ploop;
 
-    public Ploop(String n,HardwareMap hardwareMap){
+    public Ploop(String n, HardwareMap hardwareMap) {
         super(n, hardwareMap);
+        ploop = hardwareMap.get(DcMotor.class, "ploop");
     }
 
-    public void init(){
-
-    }
-
-    public void start(){
+    public void init() {
 
     }
 
-    public void stop(){
+    public void lower() {
+        ploop.setPower(1);
+    }
 
+    public void raise() {
+        ploop.setPower(-1);
+    }
+
+    public void stop() {
+        ploop.setPower(0);
     }
 }
